8762574a2d029e53723e99e0919051968584fed1
Anshuman Nene <anshuman.nene@outlook.com>
2015-04-10 01:30:23 -0700

    implemented AppDirectApi needed for oauth

---
+++ test/unit/integrationchallenge/AppDirectApiTests.groovy
@@ -0,0 +1,17 @@
+package integrationchallenge
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(AppDirectApi)
+class AppDirectApiTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
