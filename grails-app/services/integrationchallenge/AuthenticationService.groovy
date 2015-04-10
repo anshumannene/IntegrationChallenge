b72a277f2a0beecc67ec92f73698e70352840979
Anshuman Nene <anshuman.nene@outlook.com>
2015-04-10 01:43:04 -0700

    passed in the correct OAuthService instance while signing and sending a request

--- grails-app/services/integrationchallenge/AuthenticationService.groovy
+++ grails-app/services/integrationchallenge/AuthenticationService.groovy
@@ -4,18 +4,19 @@ import org.scribe.model.OAuthRequest
 import org.scribe.model.Response
 import org.scribe.model.Token
 import org.scribe.model.Verb
+import org.scribe.oauth.OAuthService
 
 import uk.co.desirableobjects.oauth.scribe.OauthResourceService
-import uk.co.desirableobjects.oauth.scribe.OauthService
 
-class AuthenticationService extends OauthService {
+class AuthenticationService {
 
     OauthResourceService oauthResourceService
-    
+    OAuthService service
+
     def signAndSendRequest(url) {
         OAuthRequest request = new OAuthRequest(Verb.GET, url)
         Token token = new Token("", "")
-        Response response = oauthResourceService.signAndSend(this, new Token("", ""), request)
+        Response response = oauthResourceService.signAndSend(service, new Token("", ""), request)
         response.getBody()
     }
 }
