1a140b0d200041018190e99730f2ec6433d06670
Anshuman Nene <anshuman.nene@outlook.com>
2015-04-12 04:31:59 -0700

    updated spring config

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,19 +1,14 @@
 import grails.util.Environment
-import integrationchallenge.AuthenticationService
 import integrationchallenge.MockSubscriptionService
-import integrationchallenge.SubscriptionService
 
 beans = {
-
     switch(Environment.getCurrentEnvironment()) {
         case Environment.TEST:
             subscriptionService(MockSubscriptionService)
             break
         case Environment.DEVELOPMENT:
-            subscriptionService(SubscriptionService) { authenticationService(AuthenticationService) }
             break
         case Environment.PRODUCTION:
-            subscriptionService(SubscriptionService) { authenticationService(AuthenticationService) }
             break
     }
 }
